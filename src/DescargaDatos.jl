b6f76e8d3cefd2f766c5a9facaf77402f3f35938
Chrissart <68823371+Chrissart@users.noreply.github.com>
2021-02-03 12:42:39 -0600

    Add files via upload

--- src/DescargaDatos.jl
+++ src/DescargaDatos.jl
@@ -9,7 +9,7 @@ export descargar_datos
 global_logger(TerminalLogger(right_justify=120))
 
 #Modulo para descomprimir archivos de python
-zipfile = pyimport("zipfile")
+zipfile = pushfirst!(PyVector(pyimport("sys")."path"), "zipfile")
 
 struct Descargable
             path::String
